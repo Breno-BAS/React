65643dd8cb83791b7f57a6f7a6151d159f72d726
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-10-18 15:05:53 -0300

    tentano add

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -2,9 +2,12 @@ const Page = () => {
   return (
     <div className="container mx-auto">
 
-      <div className="flex justify-center items-center">
-        
-      </div>
+    <div className="rounded-xl overflow-hidden p-6 bg-slate-800">
+      <div className="w-24 h-24 bg-cover bg-center rounded-full mx-auto bg-[url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/BenFranklinDuplessis.jpg/250px-BenFranklinDuplessis.jpg)]"></div>
+    
+      <div className="text-white text-center " >"Me fale e eu esqueço. Me ensine e eu lembro. Me envolva e eu aprendo de verdade."</div>
+      <div className="text-white text-center mb-5">Benjamin Flankin</div>
+    </div>
 
     </div>
   )
