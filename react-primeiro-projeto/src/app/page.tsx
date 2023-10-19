25ddc9c37a0dd086bb42c7c1f4bd0ca19857021d
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-10-19 14:49:38 -0300

    Card Hero

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -2,12 +2,8 @@ const Page = () => {
   return (
     <div className="container mx-auto">
 
-    <div className="rounded-xl overflow-hidden p-6 bg-slate-800">
-      <div className="w-24 h-24 bg-cover bg-center rounded-full mx-auto bg-[url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/BenFranklinDuplessis.jpg/250px-BenFranklinDuplessis.jpg)]"></div>
-    
-      <div className="text-white text-center " >"Me fale e eu esqueço. Me ensine e eu lembro. Me envolva e eu aprendo de verdade."</div>
-      <div className="text-white text-center mb-5">Benjamin Flankin</div>
-    </div>
+      <div className="border-2 bg-green-500 w-8 h-8"></div>
+
 
     </div>
   )
