d19b04e2f2ed011ac162293e59f5ecce68458d01
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-10-22 12:55:33 -0300

    aprendendo react

---
+++ react-primeiro-projeto/src/app/components/Square.tsx
@@ -0,0 +1,7 @@
+export const Square = () => {
+    return (
+        <div className="w-52 h-52 bg-orange-400 text-white">
+            Texto Qualquer inspirador.
+        </div>
+    );
+}
--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,12 +1,28 @@
+import { Person } from "./components/Person";
+
 const Page = () => {
   return (
-    <div className="container mx-auto">
+    <div>
+      <h1 className="font-bold text-2xl">Olá Mundo</h1>
+      <h3>Algum outro texto</h3>
 
-      <div className="border-2 bg-green-500 w-8 h-8"></div>
-
-
+      <Person
+        name = 'Elon Musk'
+        avatar = 'https:// classic. exame. com/wp-content/uploads/2021/04/Elon-Musk.jpg'
+        roles = {['Ceo da Tesla', 'Ceo da SpaceX']}
+      />
+      <Person
+        name = 'Jeff Bezos'
+        avatar = 'https://www.bloomberglinea.com/resizer/lYzExXPogs0LIafyJ5YsuaA0-4k=/800x0/filters:format(webp):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/bloomberglinea/XJIAZDYBVBFD5LL4NZNHRRIQLM.jpg'
+        roles = {['Ceo da Amazon', 'Ceo da Blue Origin']}
+      />
+      <Person
+        name = 'Pedro Ricardo'
+        roles = {['Ceo da Porra toda', 'Ceo de Porra Nenhuma']}
+      />
     </div>
-  )
+    
+  );
 };
 
 export default Page;
