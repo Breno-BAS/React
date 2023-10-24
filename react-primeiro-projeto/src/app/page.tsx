f24a0819e12f6ea36b6a93b0e07d452c1bd6b75d
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-10-24 19:40:51 -0300

    + 1 dia

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Person } from "./components/Person";
+import { Card } from "./components/Card";
 
 const Page = () => {
   return (
@@ -6,20 +6,8 @@ const Page = () => {
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <Person
-        name = 'Elon Musk'
-        avatar = 'https:// classic. exame. com/wp-content/uploads/2021/04/Elon-Musk.jpg'
-        roles = {['Ceo da Tesla', 'Ceo da SpaceX']}
-      />
-      <Person
-        name = 'Jeff Bezos'
-        avatar = 'https://www.bloomberglinea.com/resizer/lYzExXPogs0LIafyJ5YsuaA0-4k=/800x0/filters:format(webp):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/bloomberglinea/XJIAZDYBVBFD5LL4NZNHRRIQLM.jpg'
-        roles = {['Ceo da Amazon', 'Ceo da Blue Origin']}
-      />
-      <Person
-        name = 'Pedro Ricardo'
-        roles = {['Ceo da Porra toda', 'Ceo de Porra Nenhuma']}
-      />
+      <Card phrase="Alguma frase TOP" author="" />
+
     </div>
     
   );
