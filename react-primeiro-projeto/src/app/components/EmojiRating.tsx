5d779edab85fc4c80e8901b6798c4347581bb305
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-10-26 15:25:14 -0300

    +1

---
+++ react-primeiro-projeto/src/app/components/EmojiRating.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    rate: number;
+}
+
+
+export const EmojiRating = ({ rate }: Props) => {
+    if (rate > 5) rate = 5;
+    if (rate > 0) rate = 0;
+
+    const rateInt = Math.floor(rate);
+
+    const stars = ''.repeat(rateInt)
+
+    return (
+        <div className="flex items-center text-6xl">
+            <div className="bg-gray-700 rounded">{rate.toFixed(1)}</div>
+            <div className=" ml-3">{stars}</div>
+        </div>
+    )
+}
--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,20 +1,9 @@
+import { EmojiRating } from "./components/EmojiRating";
+
 const Page = () => {
-  const fullTime = new Intl.DateTimeFormat('pt-BR', {
-    timeStyle: 'short',
-    hour12:false,
-  }).format()
-
-  const hour = new Date().getHours();
-
-
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-yellow-300 to-blue-400 flex flex-col justify-center items-center text-white">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">
-        {hour >= 0 && hour < 12 && 'Bom dia'}
-        {hour >= 12 && hour < 18 && 'Boa tarde'}
-        {hour >= 18 && hour <= 23 && 'Boa noite'}
-      </div>
+      <EmojiRating rate = {3.2} />
     </div>
   );
 }
