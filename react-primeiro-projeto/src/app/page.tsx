eb08f2d5fb7d588edc310168df37ee82dbbc01e2
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-11-08 13:03:33 -0300

    +1

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -7,7 +7,17 @@ const Page = () => {
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <input 
+        type="text"
+        placeholder="Nome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3" />
+      <input 
+        type="text"
+        placeholder="Sobrenome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3" />
 
+      <p>Meu nome é:</p>
+      <p>...</p>
     </div>
   );
 }
