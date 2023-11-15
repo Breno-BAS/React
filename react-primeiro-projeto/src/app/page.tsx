316ff80dd924b1882ecb4871819e364e1ba6f495
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-11-15 14:08:18 -0300

    +1 dia de estudo

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,23 +1,34 @@
 "use client"
 
 import { useState } from "react";
+import { Personn } from "./types/Personn";
 
 
 const Page = () => {
+  const [fullName, setFullName] = useState<Personn>({name: 'Breno', lastName: 'Andrade'});
 
+    const handleClearBUtton = () => {
+      setFullName({ ...fullName, name: '' })
+    }
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
       <input 
         type="text"
         placeholder="Nome"
-        className="border border-black p-3 text-2xl text-black rounded-md mb-3" />
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3" 
+        value={fullName.name}
+        onChange={e => setFullName({ ...fullName, name: e.target.value })}/>
       <input 
         type="text"
         placeholder="Sobrenome"
-        className="border border-black p-3 text-2xl text-black rounded-md mb-3" />
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3" 
+        value={fullName.lastName}
+        onChange={e => setFullName({ ...fullName, lastName: e.target.value })}/>
 
-      <p>Meu nome é:</p>
-      <p>...</p>
+      <p>Meu nome completo é:</p>
+      <p>{fullName.name} {fullName.lastName}</p>
+
+      <button onClick={handleClearBUtton}>Limpar nome</button>
     </div>
   );
 }
---
+++ react-primeiro-projeto/src/app/types/Personn.ts
@@ -0,0 +1,4 @@
+export type Personn = {
+    name: string;
+    lastName: string;
+}
