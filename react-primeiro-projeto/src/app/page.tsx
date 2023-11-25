223eb7151fe71f1bbc54763922e9b530080c92e4
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-11-24 21:05:15 -0300

    Estudando UseEffect

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,69 +1,23 @@
 "use client"
 
-import { useState } from "react";
-import { TodoItem } from "./types/TodoItem";
-import { Input } from "postcss";
-
+import { useEffect, useState } from "react";
 
 const Page = () => {
-  const [itemInput, setItemInput] = useState('');
-  const [list, setList] = useState<TodoItem[]>([
-    {id: 1, label: 'Fazer dever de casa', checked: true },
-    {id: 2, label: 'Comprar o bolo', checked: false }
-  ]);
+  const [name, setName] = useState('Breno');
 
-  const handleAddButton = () => {
-    if(itemInput.trim() === '') return;
+  useEffect(() => {
+    console.log('Rodou o effect')
+  });
 
-    setList([...list, { id: list.length + 1, label: itemInput, checked: false}]);
-    setItemInput('')
-  }
 
-  const deleteItem = (id: number) => {
-    setList(
-      list.filter(item => item.id !== id));
-  }
+  return(
+    <div className="">
+      <p>Meu nome é {name}</p>
 
-  const toggleItem = (id: number) => {
-    let newList = [...list];
-
-    for(let i in newList) {
-      if(newList[i].id === id) {
-        newList[i].checked = !newList[i].checked;
-      }
-    }
-
-    setList(newList);
-  }
-  
-
- return (
-  <div className="w-screen h-screen flex flex-col items-center text-2xl">
-    <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
-
-    <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-500 text-white">
-      <input 
-        type="text" 
-        placeholder="o que deseja fazer?"
-        className="flex-1 border border-black p-3 text-2xl text-black rounded-md mr-3"
-        value={itemInput}
-        onChange={e => setItemInput(e.target.value)}/>
-        <button onClick={handleAddButton}>Adicionar</button>
+      <button onClick={() => {setName('Sarah')}} className="">Mudar para Sarah</button>
+      <button onClick={() => {setName('Jonas')}} className="">Mudar para Jonas</button>
     </div>
-
-    <p className="my-4">{list.length}Itens na lista</p>
-
-    <ul className="w-full max-w-lg list-disc pl-5">
-      {list.map((item) => (
-        <li key={item.id}>
-          <input onClick={() => toggleItem(item.id)} type="checkbox" checked= {item.checked} className="w-6 h-6 mr-3" />
-          { item.label } - <button onClick={() => deleteItem (item.id)} className="hover:underline"> [ deletar ]</button></li>
-      ))}
-
-    </ul>
-
-  </div>
- );
+  );
 }
 
 export default Page;
