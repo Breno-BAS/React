303a79b385225db89d2ee3e0aabda742a2aa6bca
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-12-18 14:07:16 -0300

    context

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,24 +1,11 @@
 "use client"
 
-import { useReducer, useState } from "react";
-import { Item } from "./types/item";
-import { listReducer } from "./reducers/listReducer";
-
 
 const Page = () => {
-  const [list, dispatch] = useReducer(listReducer, []);
 
-  const handleAddClick = () => {
-    dispatch ({
-      type: 'add',
-      payload: {
-        text: 'Novo item'
-      }
-    });
-  }
   return(
-    <div className="">
-      <button onClick={handleAddClick}>Adicionar</button>
+    <div className="container">
+      ...
     </div>     
   );
 }
