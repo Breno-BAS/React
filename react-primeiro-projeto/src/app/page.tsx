f2fb4e89de86b39a5123a7eecc6cf603b96c8241
Breno Andrade Silva <engbrenoandrade@gmail.com>
2023-12-19 14:02:38 -0300

    entendendo contexto

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,13 +1,17 @@
-"use client"
+"use client";
 
+import { Header } from "./components/Header";
+import { CountProvider } from "./contexts/CountContext";
 
 const Page = () => {
 
-  return(
-    <div className="container">
-      ...
-    </div>     
+  return (
+    <div className="mx-auto container">
+      <CountProvider>
+        <Header/>
+      </CountProvider>
+    </div>
   );
-}
+};
 
 export default Page;
